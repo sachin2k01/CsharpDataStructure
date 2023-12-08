a8df8dc3f29b02624e93a644dd0d77ac1e1f75bf
sachin2k01 <thenamesachin@gmail.com>
2023-12-08 23:53:32 +0530

    Linked list some basic operation is done

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class LinkedList
+    {
+        private Node head;
+        private int llsize;
+
+        public LinkedList() 
+        {
+            this.head = null;
+            this.llsize = 0;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.llsize == 0;
+            }
+        }
+
+        public int LinkedListSize
+        {
+            get 
+            {
+                return this.llsize;
+            }
+        }
+    }
+}
---
+++ DataStructure/LinkedListDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedListDS
+    {
+        static void Main(string[] args)
+        {
+            LinkedList ls= new LinkedList();
+            Console.WriteLine(ls.IsEmpty);
+            Console.WriteLine(ls.LinkedListSize);
+
+        }
+    }
+}
