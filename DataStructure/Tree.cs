af7c9057990e97cff05e566af5b1c87aeec66682
sachin2k01 <thenamesachin@gmail.com>
2023-12-11 11:24:26 +0530

    Tree datastructure and its traversal is implemented

---
+++ DataStructure/Tree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class TNode
+    {
+        public int data;
+        public TNode left;
+        public TNode right;
+
+        public void show()
+        {
+            Console.WriteLine($"[ {data} ]");
+        }
+
+    }
+    public class Tree
+    {
+        public TNode root;
+        public Tree( )
+        {
+            root = null;
+
+        }
+        public TNode ReturnRoot( )
+        {
+            return root;
+        }
+
+        public void Insert(int x)
+        {
+            TNode newnode=new TNode();
+            newnode.data = x;
+            if( root == null ) 
+            {
+                root=newnode;
+            }
+            else
+            {
+                TNode current=root;
+                TNode parent;
+                while(true)
+                {
+                    parent=current;
+                    if(x<current.data)
+                    {
+                        parent.left = newnode;
+                        return;
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if(current==null)
+                        {
+                            parent.right = newnode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Pre_0rder(TNode Root)
+        {
+            if( Root != null )
+            {
+                Console.Write(Root.data + " ");
+                Pre_0rder(Root.left);
+                Pre_0rder(Root.right);
+            }
+        }
+
+        public void In_order(TNode Root)
+        {
+            if( Root != null )
+            {
+                In_order(Root.left);
+                Console.Write(Root.data + " ");
+                In_order(Root.right);
+            }
+        }
+        public void Post_order(TNode Root)
+        {
+            if ( Root != null )
+            {
+                Post_order(Root.left);
+                Post_order(Root.right);
+                Console.Write(Root.data + " ");
+            }
+        }
+    }
+}
---
+++ DataStructure/TreeDS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class TreeDS
+    {
+        static void Main(string[] args)
+        {
+            Tree t = new Tree();
+            t.Insert(34);
+            t.Insert(23);
+            t.Insert(76);
+            t.Insert(56);
+            t.Insert(84);
+            t.Insert(99);
+            t.Insert(11);
+
+            Console.WriteLine("InOrder Traversal of a Tree");
+            t.In_order(t.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("PreOrder Traversal of a Tree");
+            t.Pre_0rder(t.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("PostOrder Traversal of a Tree");
+            t.Post_order(t.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+        }
+    }
+}
